9c9d8434c87a7bb98847d0330c105d63c98b2217
milaynedev <milaynethamyris@gmail.com>
2021-04-25 15:23:24 -0300

    criacao de arquivos de auxilio de instalacao e dicas para melhor funcionamento da aplicacao, baseados na segunda aula do NLW#05

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+    const currentDate =format(new Date(), 'EEEE, d MMM', {
+        locale: ptBR,
+    });
+
+
+    return(
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="Podcaster" />
+
+
+            <p>O melhor para voce ouvir, sempre!</p>
+
+            <span>{currentDate}</span>
+        </header>
+    );
+}
